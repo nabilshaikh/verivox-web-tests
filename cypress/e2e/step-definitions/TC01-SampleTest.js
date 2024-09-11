76363b66fe36008806c983390c3837df88598c87
Nabil <nabilshaikh26@gmail.com>
2024-09-11 18:08:47 +0530

    Corrected stylistic errors after configuring lint

--- cypress/e2e/step-definitions/TC01-SampleTest.js
+++ cypress/e2e/step-definitions/TC01-SampleTest.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="cypress"/>
 
-import { Given} from '@badeball/cypress-cucumber-preprocessor';
+import { Given } from '@badeball/cypress-cucumber-preprocessor';
 
 Given('I visit {string} page', (pageName) => {
   cy.visit(pageName);
